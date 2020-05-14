8f4b14a4ec493071ec35965a7ffc756f5ed42f4a
Robert Leduc <rleduc34@gmail.com>
2020-05-14 00:55:34 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,8 @@
-# Write your code here.
+def badge_maker (speakers)
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+room = 7
+speakers.each do |speaker|
+  puts "Hello, #{speaker}! You'll be assigned to room #{room}"
+room += 1
+  end
+end
