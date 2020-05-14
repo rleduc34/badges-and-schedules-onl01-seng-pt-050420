040fd74b6ddd87960265eee234395a4287ce8dab
Robert Leduc <rleduc34@gmail.com>
2020-05-14 01:16:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,5 @@
-def badge_maker (names)
-  name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  name.each do |name|
-    puts "Hello, my name is #{name}."
-end
+def badge_maker(name)
+    puts "Hello, my name is  #{name}."
 end
 
 def batch_badge_creator(names)
