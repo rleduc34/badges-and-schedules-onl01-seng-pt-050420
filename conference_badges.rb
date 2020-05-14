cfd5e6867c41ee67747324c82e7efdcf3be59633
Robert Leduc <rleduc34@gmail.com>
2020-05-14 01:22:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,12 +8,4 @@ def batch_badge_creator(names)
   puts "Hello, my name is #{names}."
 end
 
-def batch_badge_creator(names)
-    name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-  room = 1
-  name.each do |name|
-  puts "Hello, #{name}! You'll be assigned to room #{room}"
-  room += 1
-end
-end
